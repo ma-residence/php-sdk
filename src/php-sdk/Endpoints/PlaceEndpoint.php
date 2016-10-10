a5f909366cda5e72c2cf5bbfc8e7eae4eff031cb
LaurentBrieu <laurentbrieu@gmail.com>
2016-10-10 16:35:34 +0200

    Add pagination params

--- src/php-sdk/Endpoints/PlaceEndpoint.php
+++ src/php-sdk/Endpoints/PlaceEndpoint.php
@@ -5,13 +5,17 @@ namespace MR\SDK\Endpoints;
 class PlaceEndpoint extends Endpoint
 {
     /**
+     * @param int $page
+     * @param int $per_page
      * @param $placeId
-     *
      * @return \MR\SDK\Transport\Response
      */
-    public function getActivity($placeId)
+    public function getActivity($page = 1, $per_page = 20,  $placeId)
     {
-        return $this->request->get('/places/'.$placeId.'/activity');
+        return $this->request->get('/places/'.$placeId.'/activity', [
+            'page' => $page,
+            'per_page' => $per_page,
+        ]);
     }
 
     /**
