642bfbd3ad0e676fa24e4240e53003fca7facb62
LaurentBrieu <laurentbrieu@gmail.com>
2016-10-21 17:23:51 +0200

    refactor(endpoint): Refactor PlaceEndpoint

--- src/php-sdk/Endpoints/PlaceEndpoint.php
+++ src/php-sdk/Endpoints/PlaceEndpoint.php
@@ -7,20 +7,19 @@ class PlaceEndpoint extends Endpoint
     /**
      * @param int    $page
      * @param int    $per_page
-     * @param string $placeId
      *
      * @return \MR\SDK\Transport\Response
      */
-    public function getActivity($page = 1, $per_page = 20, $placeId)
+    public function getActivity($page = 1, $per_page = 20)
     {
-        return $this->request->get('/places/'.$placeId.'/activity', [
+        return $this->request->get('/places/activity', [
             'page' => $page,
             'per_page' => $per_page,
         ]);
     }
 
     /**
-     * @param array $placeId
+     * @param string $placeId
      *
      * @return \MR\SDK\Transport\Response
      */
