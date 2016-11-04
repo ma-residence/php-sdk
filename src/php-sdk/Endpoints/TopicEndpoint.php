c53b6e5605fa75297fc72bc84308b0682d4e3c25
LaurentBrieu <laurentbrieu@gmail.com>
2016-11-04 16:46:28 +0100

    enhancement(topic): Add topic activity

--- src/php-sdk/Endpoints/TopicEndpoint.php
+++ src/php-sdk/Endpoints/TopicEndpoint.php
@@ -11,4 +11,19 @@ class TopicEndpoint extends Endpoint
     {
         return $this->request->get('/topics');
     }
+
+    /**
+     * @param string $id
+     * @param int    $page
+     * @param int    $per_page
+     *
+     * @return \MR\SDK\Transport\Response
+     */
+    public function getActivity($id, $page = 1, $per_page = 20)
+    {
+        return $this->request->get("/topics/$id/activity", [
+            'page' => $page,
+            'per_page' => $per_page,
+        ]);
+    }
 }
