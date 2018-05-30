cd21665f33631d85dbe1508a8fcd12ae82a035ad
piwi <pierre-yves.cariou@ma-residence.fr>
2018-05-30 16:09:46 +0200

    feature(MemberOf): add getMembersByType

--- src/php-sdk/Endpoints/Traits/MembersTrait.php
+++ src/php-sdk/Endpoints/Traits/MembersTrait.php
@@ -15,4 +15,12 @@ trait MembersTrait
             'per_page' => $perPage,
         ], $extraParams));
     }
+
+    public function getMembersByType(string $id, string $modelType, int $page = 1, int $perPage = 20, array $extraParams = []): Response
+    {
+        return $this->request->get("/{$this::getBaseUri()}/$id/members/$modelType", array_merge([
+            'page' => $page,
+            'per_page' => $perPage,
+        ], $extraParams));
+    }
 }
