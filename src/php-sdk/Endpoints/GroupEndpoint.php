b9b7bce9a5d797d01c6b3c591f23153369c93a91
Pascal Filipovicz <filipovi@frozenk.net>
2022-03-25 11:37:24 +0100

    fix(MAN-2399): add getGroupActivity

--- src/php-sdk/Endpoints/GroupEndpoint.php
+++ src/php-sdk/Endpoints/GroupEndpoint.php
@@ -28,6 +28,73 @@ class GroupEndpoint extends Endpoint implements ResourceEndpointInterface, Setti
         ], $extraParams));
     }
 
+    /**
+     * @param array $data
+     *
+     * @return \MR\SDK\Transport\Response
+     */
+    public function join(array $data = [])
+    {
+        return $this->request->post('/groups/join', [], $data);
+    }
+
+    /**
+     * @param string $id
+     *
+     * @return \MR\SDK\Transport\Response
+     */
+    public function getDirectory($id)
+    {
+        return $this->request->get('/groups/' . $id . '/directory');
+    }
+
+    /**
+     * @param string $id
+     * @param array  $data
+     *
+     * @return \MR\SDK\Transport\Response
+     */
+    public function postDirectory($id, array $data = [])
+    {
+        return $this->request->post('/groups/' . $id . '/directory', [], $data);
+    }
+
+    /**
+     * @param string $id
+     * @param string $directoryId
+     * @param array  $data
+     *
+     * @return \MR\SDK\Transport\Response
+     */
+    public function putDirectory($id, $directoryId, array $data = [])
+    {
+        return $this->request->put('/groups/' . $id . '/directory/' . $directoryId, [], $data);
+    }
+
+    /**
+     * @param string $id
+     * @param string $directoryId
+     * @param array  $data
+     *
+     * @return \MR\SDK\Transport\Response
+     */
+    public function patchDirectory($id, $directoryId, array $data = [])
+    {
+        return $this->request->patch('/groups/' . $id . '/directory/' . $directoryId, [], $data);
+    }
+
+    /**
+     * @param string $id
+     * @param string $directoryId
+     * @param array  $data
+     *
+     * @return \MR\SDK\Transport\Response
+     */
+    public function deleteDirectory($id, $directoryId, array $data = [])
+    {
+        return $this->request->delete('/groups/' . $id . '/directory/' . $directoryId, [], $data);
+    }
+
     public static function getBaseUri(): string
     {
         return 'groups';
--- src/php-sdk/Endpoints/HallNumericEndpoint.php
+++ src/php-sdk/Endpoints/HallNumericEndpoint.php
@@ -15,6 +15,22 @@ class HallNumericEndpoint extends Endpoint implements ResourceEndpointInterface
      *
      * @return \MR\SDK\Transport\Response
      */
+    public function getGroupActivity($id, $page = 1, $perPage = 20, $extraParams = [])
+    {
+        return $this->request->get("/{$this::getBaseUri()}/$id/group/activity", array_merge([
+            'page' => $page,
+            'per_page' => $perPage,
+        ], $extraParams));
+    }
+
+    /**
+     * @param string $id
+     * @param int    $page
+     * @param int    $perPage
+     * @param array  $extraParams
+     *
+     * @return \MR\SDK\Transport\Response
+     */
     public function getLotActivity($id, $page = 1, $perPage = 20, $extraParams = [])
     {
         return $this->request->get("/{$this::getBaseUri()}/$id/lot/activity", array_merge([
