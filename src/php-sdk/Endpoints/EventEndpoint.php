ced83d5cb8db2f5f58d9bc420207751c9f19d20a
Pascal Filipovicz <pascal.filipovicz@frozenk.net>
2022-05-10 09:33:30 +0200

    fix(MAN-2542): add Event::getRecipients endpoint

--- src/php-sdk/Endpoints/EventEndpoint.php
+++ src/php-sdk/Endpoints/EventEndpoint.php
@@ -2,9 +2,16 @@
 
 namespace MR\SDK\Endpoints;
 
+use MR\SDK\Transport\Response;
+
 class EventEndpoint extends Endpoint implements ResourceEndpointInterface
 {
     use Traits\ResourceTrait;
+
+    public function getRecipients(string $id) : Response
+    {
+        return $this->request->get("/{$this::getBaseUri()}/$id/recipients");
+    }
 
     public static function getBaseUri(): string
     {
